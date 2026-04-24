28ed8e2ff3532201d7431c6acdb62fbb9c17c454
genuinelycool <genuinelycool@gmail.com>
2026-04-24 12:23:41 +0545

    How to Pass Props in React | Data Flow Between Components | React 19 - Ep 05

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
+import { UserCard } from "./components/feature/UserCard";
 import Header from "./components/layout/Header";
+import { Button } from "./components/ui/Button";
 
 function App() {
+  const firstName = "Programming Fields";
+  const technology = "React";
+
+  const user = {
+    firstName: "Programming Fields",
+    technology: "Full stack Devs.",
+  };
+
+  function showAlert() {
+    alert("Alert from Parent");
+  }
+
   return (
     <>
-      <Header />
-      <h1>Hey this is App component!</h1>
+      {/* <Header />
+      <h1>Hey this is App component!</h1> */}
+
+      {/* Passing STATIC PROPS */}
+      {/* <UserCard firstname="Programming Fields" technology="React" /> */}
+
+      {/* Passing DYNAMIC PROPS */}
+      <UserCard firstname={firstName} technology={technology} />
+      <UserCard firstname={user.firstName} technology={user.technology} />
+      <UserCard />
+
+      {/* <Button clickHandle={ () => alert('Alert from Parent') } /> */}
+      <Button clickHandle={showAlert} />
     </>
   );
 }
--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,5 +1,12 @@
-export const Button = ({label}) => {
+export const Button = ({ label = "Click Me", clickHandle }) => {
   return (
-    <button>{label || "Click Me"}</button>
+    // <button onClick={() => alert('hello')}>{ label }</button>
+    <button onClick={clickHandle}>{label}</button>
   );
 };
+
+// export const Button = ({label}) => {
+//   return (
+//     <button>{label || "Click Me"}</button>
+//   );
+// };
